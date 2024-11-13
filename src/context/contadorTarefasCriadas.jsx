94942a575f6a2ae2e29c5b4d934df8f4bb66ec24
Sara Guaiume <130069044+Murilo-R-Robetti@users.noreply.github.com>
2024-11-13 10:50:13 -0300

    .

--- src/context/contadorTarefasCriadas.jsx
+++ src/context/contadorTarefasCriadas.jsx
@@ -1,20 +1,20 @@
-import React, { createContext, useState } from 'react';
+// import React, { createContext, useState } from 'react';
 
-//CRIACAO DO CONTEXTO PARA TAREFAS CRIADAS
-export const contadorTarefasCriadas =createContext();
+// //CRIACAO DO CONTEXTO PARA TAREFAS CRIADAS
+// export const contadorTarefasCriadas =createContext();
 
-export function contadorTarefasCriadas({children}){
+// export function contadorTarefasCriadas({children}){
 
-    const [contador, setContador] = useState(0);
-    const [conta, setConta] = useState(1);
+//     const [contador, setContador] = useState(0);
+//     const [conta, setConta] = useState(1);
 
-    const contando = () => {setContador(contador + conta)}}
+//     const contando = () => {setContador(contador + conta)}}
 
-    const descontando = () => { setConta (conta - 1)};
+//     const descontando = () => { setConta (conta - 1)};
 
-    return (
-        <contadorTarefasCriadas.Provider value={{ contar, descontar}}>
-            {children}
-        </contadorTarefasCriadas.Provider>
-    )
+//     return (
+//         <contadorTarefasCriadas.Provider value={{ contar, descontar}}>
+//             {children}
+//         </contadorTarefasCriadas.Provider>
+//     )
 
